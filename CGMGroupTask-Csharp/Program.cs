4f6ae77d475d535f84e8f8f6a6208287ba8d3f23
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-08-15 17:55:02 +0330

    Add a loop to add and get question every time.

--- CGMGroupTask-Csharp/Program.cs
+++ CGMGroupTask-Csharp/Program.cs
@@ -9,23 +9,25 @@ Console.WriteLine("Hello, World!");
 QuestionAnswer qaSystem = new QuestionAnswer();
 MainService mainService = new MainService();
 
+while (true)
+{
+    Console.WriteLine("Choose an option: \n1. Ask a question\n2. Add a question");
+    string choice = Console.ReadLine().Trim();
 
-Console.WriteLine("Choose an option: \n1. Ask a question\n2. Add a question");
-string choice = Console.ReadLine().Trim();
-
-if (choice == "1")
-{
-    Console.WriteLine("Enter your question:");
-    string question = Console.ReadLine().Trim();
-    mainService.GetQuestion(question);
-}
-else if (choice == "2")
-{
-    Console.WriteLine("Enter your question and answers (e.g., What is Peters favorite food? \"Pizza\" \"Spaghetti\"):");
-    string input = Console.ReadLine().Trim();
-    mainService.AddQuestion(input);
-}
-else
-{
-    Console.WriteLine("Invalid option. Please select 1 or 2.");
+    if (choice == "1")
+    {
+        Console.WriteLine("Enter your question:");
+        string question = Console.ReadLine().Trim();
+        mainService.GetQuestion(question);
+    }
+    else if (choice == "2")
+    {
+        Console.WriteLine("Enter your question and answers (e.g., What is Peters favorite food? \"Pizza\" \"Spaghetti\"):");
+        string input = Console.ReadLine().Trim();
+        mainService.AddQuestion(input);
+    }
+    else
+    {
+        Console.WriteLine("Invalid option. Please select 1 or 2.");
+    }
 }
