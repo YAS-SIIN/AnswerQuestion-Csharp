c7cb7b14c0ef68e6de664dcd02e10ba09fb3cb75
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-08-15 17:16:23 +0330

    Add model and service class

---
+++ CGMGroupTask-Csharp/Models/QuestionAnswer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGMGroupTask_Csharp.Models;
+
+public class QuestionAnswer
+{
+    public string Question { get; set; }
+    public List<string> Answer { get; set; }
+
+}
