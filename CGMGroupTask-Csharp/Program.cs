d1a9e62ea347ef56e484c82a7358337f925e118d
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-08-15 21:03:54 +0330

    Added try catch block

--- CGMGroupTask-Csharp/Program.cs
+++ CGMGroupTask-Csharp/Program.cs
@@ -6,26 +6,33 @@ Console.WriteLine("Hello, World!");
 
 MainService mainService = new MainService();
 
-while (true)
+try
 {
-    Console.WriteLine("\n");
-    Console.WriteLine("Choose an option: \n1. Ask a question\n2. Add a question");
-    string choice = Console.ReadLine().Trim();
+    while (true)
+    {
+        Console.WriteLine("\n");
+        Console.WriteLine("Choose an option: \n1. Ask a question\n2. Add a question");
+        string choice = Console.ReadLine().Trim();
 
-    if (choice == "1")
-    {
-        Console.WriteLine("Enter your question:");
-        string question = Console.ReadLine().Trim();
-        mainService.GetQuestion(question);
+        if (choice == "1")
+        {
+            Console.WriteLine("Enter your question:");
+            string question = Console.ReadLine().Trim();
+            mainService.GetQuestion(question);
+        }
+        else if (choice == "2")
+        {
+            Console.WriteLine("Enter your question and answers (e.g., What is Peters favorite food? \"Pizza\" \"Spaghetti\"):");
+            string input = Console.ReadLine().Trim();
+            mainService.AddQuestion(input);
+        }
+        else
+        {
+            Console.WriteLine("Invalid option. Please select 1 or 2.");
+        }
     }
-    else if (choice == "2")
-    {
-        Console.WriteLine("Enter your question and answers (e.g., What is Peters favorite food? \"Pizza\" \"Spaghetti\"):");
-        string input = Console.ReadLine().Trim();
-        mainService.AddQuestion(input);
-    }
-    else
-    {
-        Console.WriteLine("Invalid option. Please select 1 or 2.");
-    }
+}
+catch (Exception)
+{
+    Console.WriteLine("An error occurred.");
 }
